10e824a3bb21c4c510fb4249297033f7b434ba3d
kurpau <paul.kurlavicius@gmail.com>
2023-06-29 16:19:58 +0300

    Finish assignment

---
+++ assignments/basics-assignment-1-problem/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: "Paulius",
+      age: 21,
+      imgSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png",
+    };
+  },
+}).mount("#assignment");
