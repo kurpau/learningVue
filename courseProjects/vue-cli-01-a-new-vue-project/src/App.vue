bc26f29f2cde458dac811ee9d5873f322163f7d4
kurpau <paul.kurlavicius@gmail.com>
2023-08-15 13:30:04 +0300

    Finish module

---
+++ courseProjects/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          email: "manuel@localhost.com",
+          phone: "01234 5678 991",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          email: "julie@localhost.com",
+          phone: "09876 543 221",
+        },
+      ],
+    };
+  },
+};
+</script>
