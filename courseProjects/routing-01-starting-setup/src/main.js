5735bcb29eaddf5d8f171e72fe77df69985663f7
kurpau <paul.kurlavicius@gmail.com>
2023-09-25 18:10:33 +0300

    Add watcher

--- courseProjects/routing-01-starting-setup/src/main.js
+++ courseProjects/routing-01-starting-setup/src/main.js
@@ -4,14 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
-import TeamMember from './components/teams/TeamMembers.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMember },
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
   ],
 });
 
