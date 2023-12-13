ee7dc2093bfbe7e79b3c00db4e4c6eccd667d1c0
kurpau <paul.kurlavicius@gmail.com>
2023-12-13 15:57:19 +0200

    Start adding backend

--- mainProject/src/store/modules/coaches/actions.js
+++ mainProject/src/store/modules/coaches/actions.js
@@ -1,13 +1,20 @@
 export default {
   registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
+
+    fetch(
+      `https://vue-http-demo-13a9e-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'POST',
+      }
+    );
 
     context.commit('registerCoach', coachData);
   },
