b386a4294a4840442b125d1dc595fddf1e25642a
kurpau <paul.kurlavicius@gmail.com>
2023-12-22 18:59:59 +0200

    Add backend

--- mainProject/src/store/modules/coaches/actions.js
+++ mainProject/src/store/modules/coaches/actions.js
@@ -1,5 +1,5 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
     const userId = context.rootGetters.userId;
     const coachData = {
       firstName: data.first,
@@ -9,13 +9,46 @@ export default {
       areas: data.areas,
     };
 
-    fetch(
-      `https://vue-http-demo-13a9e-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+    const response = await fetch(
+      `https://coach-app-6f60c-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
       {
-        method: 'POST',
+        method: 'PUT',
+        body: JSON.stringify(coachData),
       }
     );
 
-    context.commit('registerCoach', coachData);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to load data');
+      throw error;
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coach-app-6f60c-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   },
 };
